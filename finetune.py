39bec9a309d3ac74506e7ce18ca70e035bf70543
Chan-Y <qian2013.yu@gmail.com>
2024-04-19 16:10:39 -0400

    save predict results to file

--- finetune.py
+++ finetune.py
@@ -1,14 +1,15 @@
 import argparse
 from pathlib import Path
-from datasets import load_dataset, DatasetDict, concatenate_datasets,load_metric
-import random 
+from datasets import load_metric, load_dataset, DatasetDict, concatenate_datasets
+import random, os, json
 import nltk
+nltk.download('punkt')
 from transformers import AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorWithPadding, AutoModelForSequenceClassification
 import torch
-import os, json
-from sklearn.metrics import accuracy_score
-
 import numpy as np
+
+local_dir = 'TOBECHANGED'
+
 
 def label_to_number(example):
     ''' Convert 'positive' to 1, 'negative' to 0. '''
@@ -49,7 +50,6 @@ def tokenize_text(text, max_input_length, tokenizer):
     tokenized_text['labels'] = text['sentiment']
     return tokenized_text
 
-
 def load_data(data_dir, count_per_lang):
     '''
     Loads text data and label from csv files in the specified directory.
@@ -63,8 +63,8 @@ def load_data(data_dir, count_per_lang):
         - 'sentiment': List of labels (converted to numerical ints) associated with each text entry.
     '''
     # load two datasets
-    eng_data = load_dataset("csv", data_files=f"{data_dir}/Eng_IMDB_Dataset.csv")
-    fre_data = load_dataset("csv", data_files=f"{data_dir}/Fre_train.csv")
+    eng_data = load_dataset("csv", data_files=f"{local_dir}/{data_dir}/Eng_IMDB_Dataset.csv")
+    fre_data = load_dataset("csv", data_files=f"{local_dir}/{data_dir}/Fre_train.csv")
     # print(f"After loading: ENG {eng_data} FRE {fre_data}\n\n")
     
     # Random sampling count_per_lang number data
@@ -85,7 +85,9 @@ def load_data(data_dir, count_per_lang):
     return eng_data, fre_data
     
 def model_init():
-    return AutoModelForSequenceClassification.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    model.to(device)
+    return model
 
 def compute_metrics(eval_pred):
     metric1 = load_metric("precision")
@@ -97,12 +99,17 @@ def compute_metrics(eval_pred):
     logits = predictions[0]
     predictions = np.argmax(logits, axis=1)
 
-    precision = metric1.compute(predictions=predictions, references=labels, average="weighted")["precision"]
-    recall = metric2.compute(predictions=predictions, references=labels, average="weighted")["recall"]
-    f1 = metric3.compute(predictions=predictions, references=labels, average="weighted")["f1"]
-    accuracy = np.mean(predictions == labels) # metric4.compute(predictions=predictions, references=labels)["accuracy"]
+    precision = metric1.compute(predictions=predictions, references=labels)["precision"]
+    recall = metric2.compute(predictions=predictions, references=labels)["recall"]
+    f1 = metric3.compute(predictions=predictions, references=labels)["f1"]
+    accuracy = np.mean(predictions == labels)  
 
-    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+    return {
+        "accuracy": round(accuracy, 6), 
+        "precision": round(precision, 6), 
+        "recall": round(recall, 6), 
+        "f1": round(f1, 6)
+    }
 
 
 def train_model(model_name, tokenizer, tokenized_train, tokenized_validation, 
@@ -117,6 +124,7 @@ def train_model(model_name, tokenizer, tokenized_train, tokenized_validation,
     - data_collator (object): Data collator with padding.
     - training_args (object): Training arguments for the Trainer.
     """
+
     trainer = Seq2SeqTrainer(
         model_init=model_init,
         args=training_args,
@@ -127,20 +135,16 @@ def train_model(model_name, tokenizer, tokenized_train, tokenized_validation,
         compute_metrics=compute_metrics
     )
 
-    print(f'******************\nTraining ...\n******************')
+    print(f'************************************\n\tTraining ...\n************************************')
     trainer.train()
-    return trainer.evaluate()
+    return trainer.evaluate(), trainer
 
 
 def main(args):
     '''Main function'''
 
     count_per_lang = args.max
-    max_input_length = 256
-
-    if args.test:
-        test(args.test_model_dir, max_input_length)
-        return 
+    max_input_length = 350
 
 
     ### Load data and split into train, validation, test following 80%/10%/10%
@@ -166,6 +170,7 @@ def main(args):
     # print(eng_data, fre_data)
 
     ### Check if CUDA is available
+    global device
     if torch.cuda.is_available():
         device = torch.device("cuda")
         print("Using GPU:", torch.cuda.get_device_name(0))
@@ -177,25 +182,29 @@ def main(args):
     global model_name
     model_name = args.model_name
     print(f'\nUSING PRETRAINED MODEL: {model_name}\n')
-    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir='./models')
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=f'{local_dir}/models')
     tokenized_eng_dataset = eng_data.map(
+        lambda review: tokenize_text(review, max_input_length, tokenizer), 
+        batched=True
+    )
+    tokenized_fre_dataset = fre_data.map(
         lambda review: tokenize_text(review, max_input_length, tokenizer), 
         batched=True
     )
     # print(tokenized_eng_dataset, tokenized_eng_dataset['train'][0])
     
     ### Combine two language sets
-    combined_train = concatenate_datasets([tokenized_eng_dataset['train'], tokenized_eng_dataset['train']]).select_columns(['input_ids', 'labels'])
-    combined_validation = concatenate_datasets([tokenized_eng_dataset['validation'], tokenized_eng_dataset['validation']]).select_columns(['input_ids', 'labels'])
-    combined_test = concatenate_datasets([tokenized_eng_dataset['test'], tokenized_eng_dataset['test']]).select_columns(['input_ids', 'labels'])
-
+    combined_train = concatenate_datasets(
+        [tokenized_eng_dataset['train'], tokenized_fre_dataset['train']]).select_columns(['input_ids', 'labels'])
+    combined_validation = concatenate_datasets(
+        [tokenized_eng_dataset['validation'], tokenized_fre_dataset['validation']]).select_columns(['input_ids', 'labels'])
     
     data_collator = DataCollatorWithPadding(
         tokenizer, padding="max_length", max_length=max_input_length
     )
     
     batch_size = 8
-    model_dir = f'./models/{model_name}_moviereview_analysis'
+    model_dir = f'{local_dir}/models/{model_name}_moviereview_analysis'
     training_args = Seq2SeqTrainingArguments(
         model_dir,
         evaluation_strategy="steps",
@@ -209,15 +218,15 @@ def main(args):
         per_device_eval_batch_size=batch_size,
         weight_decay=0.01,
         save_total_limit=3,
-        num_train_epochs=10,
+        num_train_epochs=1,
         predict_with_generate=False,
         fp16=True,
         load_best_model_at_end=True,
-        report_to="tensorboard"
+        # report_to="tensorboard"
     )
 
 
-    evaluation_result = train_model(
+    evaluation_result, trainer = train_model(
         model_name,
         tokenizer, 
         combined_train, 
@@ -226,34 +235,68 @@ def main(args):
         training_args
     )
 
-    print(evaluation_result)
+    save_to_file(args.output_dir, f'{model_name}_{args.max}_evaluation_result.out', evaluation_result)
+
+    if args.test_set:
+        print(f'************************************\n\tTesting ...\n************************************')
+        eng_test_result = trainer.predict(test_dataset=tokenized_eng_dataset['test'].select_columns(['input_ids', 'labels']))
+        fre_test_result = trainer.predict(test_dataset=tokenized_fre_dataset['test'].select_columns(['input_ids', 'labels']))
+        save_to_file(
+            args.output_dir, 
+            f'{model_name}_{args.max}_test_result.out', 
+            {'English':eng_test_result.metrics, 'French': fre_test_result.metrics}
+        )
 
 
-
-def test(model_dir, max_input_length):
+def save_to_file(output_dir, file_name, content):
     '''
-    python3 finetuneT5.py --test --test-model-dir ./models/t5-small_moviereview_analysis/checkpoint-2000
+    Save content into a txt file in ouput_dir directory. 
+
+    Parameters:
+    - output_dir (str): The directory where the file will be saved.
+    - file_name (str): The name of the file.
+    - content (dict): The text content to be saved in the file.
     '''
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
-    model = AutoModelForSequenceClassification.from_pretrained(model_dir)   
-    text = """
-    Three words: "Cool Hand Luke." Same film, done better, done earlier.
-    For that matter, is this film any better than other Steven King "novelettes"
-    such as "Stand By Me"? All in all, it probably ranks a 6 or a 7,
-    but since people on this site have lost their minds as regards this
-    film, I give it a 1 in one man's attempt at sanity.
-    """
-
-    inputs = [text]
-
-    inputs = tokenizer(inputs, max_length=max_input_length, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs)
-        predictions = outputs.logits
-    probabilities = torch.nn.functional.softmax(predictions, dim=-1)
-    predicted_class_id = probabilities.argmax(dim=-1).item()
-    print(predicted_class_id)
+    # Create output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
     
+    # Define the complete path of the file
+    file_path = os.path.join(output_dir, file_name)
+    
+    # Open the file in write mode and save the content
+    with open(file_path, 'w') as file:
+        json.dump(content, file, ensure_ascii=False, indent=4)
+    print('---------------------------------------')
+    print(f"File saved successfully at {file_path}")
+    
+
+# def test(model_dir, max_input_length):
+#     '''
+#     python3 main.py --test --test-model-dir ./models/t5-small_moviereview_analysis/checkpoint-2000
+#     '''
+
+#     tokenizer = AutoTokenizer.from_pretrained(model_dir)
+#     model = AutoModelForSequenceClassification.from_pretrained(model_dir)   
+
+#     text = """
+#     Three words: "Cool Hand Luke." Same film, done better, done earlier.
+#     For that matter, is this film any better than other Steven King "novelettes"
+#     such as "Stand By Me"? All in all, it probably ranks a 6 or a 7,
+#     but since people on this site have lost their minds as regards this
+#     film, I give it a 1 in one man's attempt at sanity.
+#     """
+
+#     inputs = [text]
+
+#     inputs = tokenizer(inputs, max_length=max_input_length, truncation=True, return_tensors="pt")
+#     with torch.no_grad():
+#         outputs = model(**inputs)
+#         predictions = outputs.logits
+#     probabilities = torch.nn.functional.softmax(predictions, dim=-1)
+#     predicted_class_id = probabilities.argmax(dim=-1).item()
+#     print(predicted_class_id)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -273,7 +316,7 @@ if __name__ == "__main__":
         '-m', '--model-name',
         help='Model name',
         type=str,
-        default='t5-large'
+        default='t5-small'
     )
     parser.add_argument(
         '--max',
@@ -282,16 +325,20 @@ if __name__ == "__main__":
         default=5000
     )
     parser.add_argument(
-        '--test', 
+        '--test-set', 
         action="store_true",
-        help="Run test() method."
+        help="A flag whether evaluate on the test dataset."
     )
-    parser.add_argument(
-        '--test-model-dir',
-        type=str,
-        help='The model directory for testing (eg. "./models/t5-large_moviereview_analysis/checkpoint-2000")'
-    )
+    # parser.add_argument(
+    #     '--test', 
+    #     action="store_true",
+    #     help="Run test() method."
+    # )
+    # parser.add_argument(
+    #     '--test-model-dir',
+    #     type=str,
+    #     help='The model directory for testing (eg. "./models/t5-small_moviereview_analysis/checkpoint-2000")'
+    # )
 
     args = parser.parse_args()
-    nltk.download('punkt')
     main(args)
